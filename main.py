ff2ec49bd1370ee9b97da9b3976a30a3c1ad5cff
Jean-Marc <jean.marc.baubet@gmil.com>
2022-06-17 18:36:59 +0200

    fichier main.py initié

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+summary
+
+description
+
+:REQUIRES:
+
+:TODO:
+
+:AUTHOR:
+:ORGANIZATION:
+:CONTACT:
+:SINCE: %(date)s
+:VERSION: 0.1
+"""
+# ===============================================================================
+# PROGRAM METADATA
+# ===============================================================================
+__author__ = ''
+__contact__ = ''
+__copyright__ = ''
+__license__ = ''
+__date__ = '%(date)s'
+__version__ = '0.1'
+
+# ===============================================================================
+# IMPORT STATEMENTS
+# ===============================================================================
+from numpy import *  # IMPORTS ndarray(), arange(), zeros(), ones()
+
+set_printoptions(precision=5)
+set_printoptions(suppress=True)
+
+
+# from visual import *  # IMPORTS NumPy.*, SciPy.*, and Visual objects (sphere, box, etc.)
+# import matplotlib.pyplot as plt  # plt.plot(x,y)  plt.show()
+# from pylab import *  # IMPORTS NumPy.*, SciPy.*, and matplotlib.*
+# import os  # os.walk(basedir) FOR GETTING DIR STRUCTURE
+# import pickle  # pickle.load(fromfile)  pickle.dump(data, tofile)
+# from tkFileDialog import askopenfilename, askopenfile
+# from collections import namedtuple
+# from ctypes import *
+# import glob
+# import random
+# import cv2
+
+# ===============================================================================
+# METHODS
+# ===============================================================================
+
+
+# ===============================================================================
+# MAIN METHOD AND TESTING AREA
+# ===============================================================================
+def main():
+    """Description of main()"""
+
+
+if __name__ == '__main__':
+    main()
